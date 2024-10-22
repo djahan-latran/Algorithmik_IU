caf9cb6a6de9ece5f374f6d9f582decb099cb12d
Djahan <djahan.latran@gmail.com>
2024-10-22 19:24:53 +0200

    initial commit

---
+++ Algorithmik/ggt.js
@@ -0,0 +1,20 @@
+let ggt = function(num1, num2) {
+    if (num1 < num2) {
+        var smaller = num1;
+        var bigger = num2;
+    } else {
+        var smaller = num2;
+        var bigger = num1;
+    }
+    if (bigger % smaller === 0) {
+        return console.log("ggt is " + smaller)
+    } else {
+        let divider = smaller;
+        while (bigger % divider !== 0 || smaller % divider !== 0) {
+            divider--;
+        }
+        return console.log("ggt is " + divider)
+    }
+}
+ggt(16,32)
+ggt(8,8)
---
+++ Algorithmik/stacks.js
@@ -0,0 +1,44 @@
+class NodeClass {
+    constructor(value) {
+        this.value = value;
+        this.next = undefined;
+    }
+}
+
+class StackClass {
+    constructor(value) {
+        const newNode = new NodeClass(value);
+        this.top = newNode
+        this.length = 1;
+    }
+    pushNode(value) {
+        const newNode = new NodeClass(value);
+        if (this.length === 0) {
+            this.top = newNode;
+        } else {
+            newNode.next = this.top;
+            this.top = newNode;
+        }
+        this.length++;
+        return this
+    }
+    popNode() {
+        if (this.length === 0) {
+            return undefined;
+        }
+        else {
+            let temp = this.top;
+            this.top = this.top.next;
+            temp.next = null;
+            this.length--;
+            return temp
+        }
+    }
+}
+
+let myStack = new StackClass(5);
+myStack.pushNode(8);
+myStack.pushNode(3);
+console.log(myStack);
+myStack.popNode();
+console.log(myStack);
